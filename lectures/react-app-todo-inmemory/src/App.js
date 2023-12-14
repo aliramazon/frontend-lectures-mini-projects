3c20c81bd6c2a23e32f199582ec7bc1ce20320bd
Ali Ramazon <mr.safarali@gmail.com>
2023-12-14 11:20:22 -0700

    Add Edit and delete capability

--- lectures/react-app-todo-inmemory/src/App.js
+++ lectures/react-app-todo-inmemory/src/App.js
@@ -8,12 +8,23 @@ class App extends React.Component {
         this.state = {
             todos: [],
             inputValue: "",
+            inputError: false,
+            showEditModal: false,
+            inputEditValue: "",
+            editingTodoId: "",
         };
     }
 
     addTodo = (e) => {
         e.preventDefault();
-        console.log(this.state.inputValue);
+
+        if (this.state.inputValue.length <= 1) {
+            this.setState({
+                inputError: true,
+            });
+
+            return;
+        }
         const newTodo = {
             id: uuid(),
             text: this.state.inputValue,
@@ -50,18 +61,84 @@ class App extends React.Component {
         this.setState({
             inputValue: value,
         });
+
+        if (value.length <= 1) {
+            this.setState({
+                inputError: true,
+            });
+        } else {
+            this.setState({
+                inputError: false,
+            });
+        }
+    };
+
+    deleteTodo = (todoId) => {
+        this.setState((prevState) => {
+            const keptTodos = prevState.todos.filter(
+                (todo) => todo.id !== todoId
+            );
+            return {
+                todos: keptTodos,
+            };
+        });
+    };
+
+    editTodo = (todoId) => {
+        this.setState({
+            showEditModal: true,
+        });
+        let todoText = "";
+        for (const todo of this.state.todos) {
+            if (todo.id === todoId) {
+                todoText = todo.text;
+                break;
+            }
+        }
+
+        this.setState({
+            inputEditValue: todoText,
+            editingTodoId: todoId,
+        });
+    };
+
+    handleInputEdit = (e) => {
+        this.setState({
+            inputEditValue: e.target.value,
+        });
+    };
+
+    submitEdit = () => {
+        this.setState((prevState) => {
+            const updatedTodos = prevState.todos.map((todo) => {
+                if (todo.id === this.state.editingTodoId) {
+                    const copy = { ...todo, text: this.state.inputEditValue };
+                    return copy;
+                }
+                return todo;
+            });
+
+            return {
+                todos: updatedTodos,
+                showEditModal: false,
+            };
+        });
     };
 
     render() {
         return (
             <main>
                 <form onSubmit={this.addTodo}>
-                    <input
-                        onChange={this.handleOnChange}
-                        value={this.state.inputValue}
-                        type="text"
-                        placeholder="What is your mind"
-                    />
+                    <div className="form-control">
+                        <input
+                            onChange={this.handleOnChange}
+                            value={this.state.inputValue}
+                            type="text"
+                            placeholder="What is your mind"
+                        />
+                        {this.state.inputError && <span>Invalid Todo</span>}
+                    </div>
+
                     <input type="submit" value="Add Todo" />
                 </form>
                 <ul>
@@ -75,6 +152,7 @@ class App extends React.Component {
                                     }`}
                                 >
                                     <span>{todo.text}</span>
+
                                     <input
                                         type="checkbox"
                                         defaultChecked={todo.isDone}
@@ -85,10 +163,30 @@ class App extends React.Component {
                                             );
                                         }}
                                     />
+                                    <button
+                                        onClick={() => this.deleteTodo(todo.id)}
+                                    >
+                                        X
+                                    </button>
+                                    <button
+                                        onClick={() => this.editTodo(todo.id)}
+                                    >
+                                        Edit
+                                    </button>
                                 </li>
                             );
                         })}
                 </ul>
+
+                {this.state.showEditModal && (
+                    <div className="modal">
+                        <input
+                            value={this.state.inputEditValue}
+                            onChange={this.handleInputEdit}
+                        />
+                        <button onClick={this.submitEdit}> Update Todo</button>
+                    </div>
+                )}
             </main>
         );
     }
